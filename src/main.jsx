59afdf4f22d8934e040613a7cf16a0dff36dba92
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-09-29 11:25:54 -0700

    Adding GTM Tags

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,6 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { BrowserRouter } from 'react-router-dom'
 
-import TagManager from 'react-gtm-module'
-
-import './index.css'
-
-const tagManagerArgs = {
-    gtmid: 'GTM-57NH23LD'
-}
-
-TagManager.initialize(tagManagerArgs)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Provider store={store}>
